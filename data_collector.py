96d1195e3dcdde6f22abddb1a54de64bdf34e6c7
Chain <chensunmac@gmail.com>
2017-11-21 16:24:53 -0500

    visualize correlation data between companies

--- data_collector.py
+++ data_collector.py
@@ -15,6 +15,10 @@ import pandas_datareader.data as web
 import fix_yahoo_finance 
 fix_yahoo_finance.pdr_override()
 
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+import numpy as np
 import time
 
 
@@ -106,7 +110,38 @@ def compile_data():
 #compile_data()
     
     
+def visualize_data():
+    # Read Data From the Main Data Set
+    df = pd.read_csv('sp500_joined_closes.csv')
+    # create dataFrame using correlation()
+    df_corr = df.corr()
+    print(df_corr.head())
+    # Save the correlation result in .csv format
+    df_corr.to_csv('sp500corr.csv')
+    # List of values from DataFrame    
+    data1 = df_corr.values
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111)    
+    heatmap1 = ax1.pcolor(data1, cmap=plt.cm.RdYlGn)
+    fig1.colorbar(heatmap1)
+    # set x and y axis ticks
+    ax1.set_xticks(np.arange(data1.shape[1]) + 0.5, minor=False)
+    ax1.set_yticks(np.arange(data1.shape[0]) + 0.5, minor=False)
+    ax1.invert_yaxis()
+    ax1.xaxis.tick_top()
     
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+    ax1.set_xticklabels(column_labels)
+    ax1.set_yticklabels(row_labels)
+    plt.xticks(rotation=90)
+    heatmap1.set_clim(-1,1)
+    plt.tight_layout()
+    #plt.savefig("correlations.png", dpi = (300))
+    plt.show()
+
+    
+visualize_data()
     
     
     
