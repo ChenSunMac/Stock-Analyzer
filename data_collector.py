2d4acd2410bc31258a1a68e15e7b2a0d181793e3
Chain <chensunmac@gmail.com>
2017-11-22 17:55:20 -0500

    do machine learning on the data sets

--- data_collector.py
+++ data_collector.py
@@ -141,7 +141,113 @@ def visualize_data():
     plt.show()
 
     
-visualize_data()
+#visualize_data()
+
+
+"""
+Pre-Process Data for Machine Learning
+    Find Features and Labels from SP500 data:
+        Features:
+        Labels: 
+            - BUY : within the next 7 days, its price goes up more than 2%
+            - SELL : within the next 10 days, its price goes down more than 2%
+            - HOLD: meh~
+"""
+def process_data_for_labels(ticker):
+    hm_days = 7
+    df = pd.read_csv('sp500_joined_closes.csv', index_col = 0)
+    tickers = df.columns.values.tolist()
+    df.fillna(0, inplace = True)
+    for i in range(1,hm_days+1):
+        df['{}_{}d'.format(ticker,i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]    
+    df.fillna(0, inplace=True)
+    return tickers, df  
+
+tickers, df = process_data_for_labels('AMD')
+
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement: 
+            return 1
+        if col < -requirement:
+            return -1
+        return 0
+    
+from collections import Counter
+def extract_featuresets(ticker):
+    """
+    @ param:
+    @ return: 
+        X: feature sets values (daily % changes for every company in the S&P 500)
+        Y: label 
+    """
+    tickers, df = process_data_for_labels(ticker)
+
+    df['{}_target'.format(ticker)] = list(map( buy_sell_hold,
+                                               df['{}_1d'.format(ticker)],
+                                               df['{}_2d'.format(ticker)],
+                                               df['{}_3d'.format(ticker)],
+                                               df['{}_4d'.format(ticker)],
+                                               df['{}_5d'.format(ticker)],
+                                               df['{}_6d'.format(ticker)],
+                                               df['{}_7d'.format(ticker)] ))
+    
+    vals = df['{}_target'.format(ticker)].values.tolist()
+    str_vals = [str(i) for i in vals]
+    print('Data spread:', Counter(str_vals))
+
+    df.fillna(0, inplace=True)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.dropna(inplace=True)    
+    
+    df_vals = df[[ticker for ticker in tickers]].pct_change()
+    df_vals = df_vals.replace([np.inf, -np.inf], 0)
+    df_vals.fillna(0, inplace=True)   
+
+    X = df_vals.values
+    y = df['{}_target'.format(ticker)].values
+    
+    return X,y,df
+
+X, y, df = extract_featuresets('AMD')
+
+
+from sklearn import svm, cross_validation, neighbors
+from sklearn.ensemble import VotingClassifier, RandomForestClassifier
+
+def do_ml(ticker):
+    X, y, df = extract_featuresets(ticker)
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,
+                                                        y,
+                                                        test_size=0.25)
+    # Using KNN Classifier
+    # clf = neighbors.KNeighborsClassifier()
+    # Using Voting Classifier instead of normal KNN
+    clf = VotingClassifier([('lsvc',svm.LinearSVC()),
+                            ('knn',neighbors.KNeighborsClassifier()),
+                            ('rfor',RandomForestClassifier())])    
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print('Accuracy:', confidence)
+    prediction = clf.predict(X_test)
+    print('predicted class counts: ', Counter(prediction))
+    
+    
+do_ml('AMD')    
+
+
+
+
+
+
+
+
+
+
+
     
     
     
