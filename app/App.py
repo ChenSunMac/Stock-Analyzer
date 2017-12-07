3989eca00d72cedbc3abedab0fec0fe680337874
Chen Sun <Chen.Sun@puretechltd.com>
2017-12-07 14:06:52 -0500

    change in app

--- app/App.py
+++ app/App.py
@@ -6,9 +6,18 @@ Created on Tue Dec  5 15:21:17 2017
 """
 
 import tkinter as tk
+from tkinter import ttk
+
+import matplotlib
+#matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+
 
 LARGE_FONT= ("Verdana", 12)
-
+style.use("ggplot")
 
 """
 The main App: 
@@ -17,17 +26,23 @@ class StockApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        
+        tk.Tk.iconbitmap(self, 'example.ico')
+        tk.Tk.wm_title(self, "Stock Analyzer App")
+        
         container = tk.Frame(self)
         container.pack(side="top", fill="both", expand = True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
-
-        self.frames = {}
-
-        frame = StartPage(container, self)
-        self.frames[StartPage] = frame
         
-        frame.grid(row=0, column=0, sticky="nsew")
+        # Container for all different frames.. e.g. pages
+        self.frames = {}
+        
+        for F in (StartPage, PageOne, PageThree):
+            frame = F(container, self)
+            self.frames[F] = frame
+            
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(StartPage)
 
@@ -47,6 +62,56 @@ class StartPage(tk.Frame):
         # add text to window 
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10) # adding paddings around to look neat
+        # Define Button here
+        button = ttk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+        button_graph = ttk.Button(self, text="Go to Graph Page",
+                            command=lambda: controller.show_frame(PageThree))
+        button_graph.pack()
+
+class PageOne(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # add text to window 
+        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        label.pack(pady=10,padx=10) # adding paddings around to look neat        
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()    
+
+class PageThree(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        # add text to window 
+        label = tk.Label(self, text="Graph Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10) # adding paddings around to look neat        
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()   
+        # plot data in background
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        
+        #make plt.show() show up on canvas 
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        
+        
+
+
+
+def qf(quickPrint):
+    print(quickPrint)
 
 
 app = StockApp()
